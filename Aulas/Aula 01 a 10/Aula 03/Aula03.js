f5072dd4a3dee6863583fe34b0b591a438fd6e19
ItalloK <itallo016@gmail.com>
2024-09-29 19:09:59 -0300

    Aula 03

---
+++ Aulas/Aula 01 a 10/Aula 03/Aula03.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let nome = "Italo"
+console.log(nome)
+nome  = 10
+console.log(nome)
+
+const curso = "Javascript"
+console.log(curso)
+
+/*curso = "teste" // Não é possivel pois isso é uma constante
+console.log(curso)*/ // Impressão
+
