1533dab3f13e9e74778a865cdd1053b4a75b03ea
ItalloK <itallo016@gmail.com>
2024-09-29 19:40:26 -0300

    Aula 05

---
+++ Aulas/Aula 01 a 10/Aula 05/Aula05.js
@@ -0,0 +1,20 @@
+"use strict"
+
+/* Operadores de Relacao:
+    >   -> Maior
+    >=  -> Maior ou Igual
+    <   -> Menor
+    <=  -> Menor ou Igual
+    ==  -> Igual ( comparacao )
+    !=  -> Diferente
+*/
+
+
+let num1 = 10, num2 = 5, num3 = 10
+
+console.log(num1 > num2) // return true
+console.log(num1 < num2) // return false
+console.log(num1 >= num3) // return true
+console.log(num1 == num3) // return true
+console.log(!(num1 == num3)) // return false --------> Operação de Negação
+console.log(num1 != num3) // return false -----------> Operação de Diferença
