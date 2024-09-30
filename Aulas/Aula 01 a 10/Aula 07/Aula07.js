34771f4190151fee18f51a5dee137e5956ee4f56
ItalloK <itallo016@gmail.com>
2024-09-29 22:24:58 -0300

    Aula 07

---
+++ Aulas/Aula 01 a 10/Aula 07/Aula07.js
@@ -0,0 +1,18 @@
+"use strict"
+
+/* Operadores logicos:
+    AND/E   == &&
+    OR/OU   == ||
+    NOT/NÃO == !
+*/
+
+
+let n1 = 10, n2 = 5, n3 = 15, n4 = 2
+
+console.log((n1>n2) && (n1>n3))
+
+if(n1>n2 && n1>n3){
+    console.log("Verdadeiro")
+}else{
+    console.log("falso")
+}
