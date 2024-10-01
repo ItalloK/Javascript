f2d67163581195f264a2e76e0fcded1e0defc759
ItalloK <itallo016@gmail.com>
2024-09-30 23:28:25 -0300

    Aula 09

--- Aulas/Aula 01 a 10/Aula 09/Aula09.js
+++ Aulas/Aula 01 a 10/Aula 09/Aula09.js
@@ -9,4 +9,10 @@ n++ // Pós incremento
 n-- // Pós Decremento
 --n // Pré Decremento
 let res = -n // inverte o valor, vira -10
-console.log(res)
+
+let n1 = 10
+let n2 = 20
+console.log(n1+n2) // Assim ele soma
+let n3 = "10"
+let n4 = "20"
+console.log(n3+n4) // Concatenação
