95744dfa5fcb79fd3740f062ef9da60cce629ae3
ItalloK <itallo016@gmail.com>
2024-09-30 23:27:09 -0300

    Aula 09

---
+++ Aulas/Aula 01 a 10/Aula 09/Aula09.js
@@ -0,0 +1,12 @@
+"use strict"
+
+/* Pré e Pós Incremento */
+
+
+let n = 10
+n++ // Pós incremento
+++n // Pré incremento
+n-- // Pós Decremento
+--n // Pré Decremento
+let res = -n // inverte o valor, vira -10
+console.log(res)
