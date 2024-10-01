97a78ce6991c2ca8260258de7588d284b0618867
ItalloK <itallo016@gmail.com>
2024-09-30 23:19:38 -0300

    Aula 08

---
+++ Aulas/Aula 01 a 10/Aula 08/Aula08.js
@@ -0,0 +1,23 @@
+"use strict"
+
+/*let n1 = 11
+let n2 = 14
+let res = n1 & n2 // Equivalencia 
+
+let n1 = 10
+let n2 = 11
+
+let res = n1 | n2 // Faz a operacao Ou no BIT A BIT
+
+let n1 = 10
+let n2 = 11
+
+let res = n1 ^ n2 // XOR ( operador logico ) no BIT a BIT*/
+
+
+let n1 = 10
+
+let res = n1 << 1 // Deslocamento para a esquerda
+console.log(res)
+res = n1 >> 1 // Deslocamento para a direita
+console.log(res)
