27ac51a937e02b52e57d4d69a44ba888faa812e7
ItalloK <itallo016@gmail.com>
2024-10-01 18:39:26 -0300

    Aula 10

---
+++ Aulas/Aula 01 a 10/Aula 10/Aula10.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let num = 10
+let res = (num%2 == 0 ? "Par" : "Impar")
+console.log(res)
+
+let num2 = 11
+let res2 = (num > num2) ? "Sim" : "Não"
+console.log(res2)
+let stt = "A"
+let stt1 = "A"
+let stt2 = "I"
+console.log(stt == "A" ? "Ativo" : "Inativo")
