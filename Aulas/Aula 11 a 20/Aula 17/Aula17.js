96ecdab3907ec0694d2e224dd5ead8854ffd72ed
ItalloK <itallo016@gmail.com>
2024-10-06 22:49:29 -0300

    Aula 17

---
+++ Aulas/Aula 11 a 20/Aula 17/Aula17.js
@@ -0,0 +1,18 @@
+"use strict"
+
+/*let n = 0
+while(n < 10){
+    console.log(n)
+    n++
+}*/
+
+
+let n = 5
+let fat = 1
+
+while(n >= 1){ // Calcula fatorial
+    fat *= n
+    n--
+    console.log(fat)
+}
+
