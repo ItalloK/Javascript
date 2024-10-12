8d0279a91a22e6b5ce92df014128a57d4a5a739b
ItalloK <itallo016@gmail.com>
2024-10-12 14:59:57 -0300

    att

--- Projetos/API/src/staff/staffget.js
+++ Projetos/API/src/staff/staffget.js
@@ -14,4 +14,21 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+    const patientId = req.params.id;
+    const sql = 'SELECT * FROM staff WHERE idstaff = ?';
+    db.query(sql, [patientId], (err, results) => {
+        if (err) {
+            console.error('Erro ao buscar Staff:', err);
+            res.status(500).json({ error: 'Erro ao buscar Staff' });
+            return;
+        }
+        if (results.length === 0) {
+            res.status(404).json({ error: 'Staff não encontrado' });
+            return;
+        }
+        res.json(results[0]);
+    });
+});
+
 module.exports = router;
