376e345d826351b2509408b03862a16c53341f0a
ItalloK <itallo016@gmail.com>
2024-10-14 10:48:39 -0300

    att

--- Projetos/API/src/outhers/outhers.js
+++ Projetos/API/src/outhers/outhers.js
@@ -26,4 +26,31 @@ router.get('/confirm/', (req, res) => {
     });
 });
 
+router.get('/proximaconsulta/', (req, res) => {
+    const sql = `
+        SELECT consultation.*, patient.cpf, patient.name 
+        FROM consultation 
+        INNER JOIN patient ON consultation.patient_id = patient.id 
+        WHERE consultation.consultation_status = 'Confirmada' 
+        ORDER BY consultation.consultation_date ASC;
+    `;
+    
+    // Executa a consulta ao banco de dados
+    db.query(sql, (err, results) => {
+        if (err) {
+            // Tratamento de erro ao executar a consulta
+            console.error("Erro ao buscar Consultas: ", err);
+            return res.status(500).json({ error: "Erro ao buscar Consultas" });
+        }
+        
+        // Verifica se há resultados e retorna
+        if (results.length === 0) {
+            return res.status(404).json({ message: "Nenhuma consulta confirmada encontrada" });
+        }
+        
+        res.json(results); // Retorna os resultados da consulta
+    });
+});
+
+
 module.exports = router;
