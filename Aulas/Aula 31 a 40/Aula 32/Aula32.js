ca918a006327f514854ab6d0435d4c485f0ebcd4
Italo Gabriel <itallo016@gmail.com>
2025-01-27 11:08:35 -0300

    Aula 32

---
+++ Aulas/Aula 31 a 40/Aula 32/Aula32.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const cursosTodos = [...document.getElementsByClassName("curso")] // transforma em array
+const cursosC1 = [...document.getElementsByClassName("c1")] 
+const cursosC2 = [...document.getElementsByClassName("c2")] 
+
+const cursoEspecial=document.getElementsByClassName("curso")[3]
+
+console.log(cursosTodos)
+console.log(cursosC1)
+console.log(cursosC2)
+console.log(cursoEspecial)
+
+cursosC2.map((el)=>{
+    el.classList.add("destaque")
+})
